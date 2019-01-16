9ca19654eea4d8cf5202eac7fcdfb0796a7ba8b6
Min Chen <min75208@gmail.com>
2019-01-16 15:12:05 -0500

    upload website

---
+++ chens/js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 48)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  //get current year for copyright
+  let year = new Date().getFullYear();
+  $('#currentYear').text(year);
+
+  $('.category').on('click', function(){
+    $('.category').removeClass('selected');
+    $(this).addClass('selected');
+  });
+});
+
